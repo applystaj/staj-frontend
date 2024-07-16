e9d8b52fea37484bfa183fa966dc9fa2e9757b1e
İLKNUR DONDURMA <dondurmailknur001@gmail.com>
2024-07-16 10:22:59 +0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import {Layout} from "../layout";
+
+
+const routes = createBrowserRouter([
+    {
+        path: '/',
+        element:<Layout/>,
+        children:[
+           
+            
+            
+
+           
+        ]
+    },
+])
+
+
+
+export default routes
