88fd6f6eb5a3d862fb3fc161a884fc846f463d81
İLKNUR DONDURMA <dondurmailknur001@gmail.com>
2024-07-22 14:28:54 +0300

    brand added

--- src/dummy-data/brands.js
+++ src/dummy-data/brands.js
@@ -1,7 +1,8 @@
-const brands = [
-  { id: 1, name: "Adidas" },
-  { id: 2, name: "Puma" },
-  { id: 3, name: "Beko" },
-  { id: 4, name: "Samsung" },
-  { id: 5, name: "Xiomi" },
+export const brandsData = [
+  { id: 1, name: "Logitech" ,imagePath:'logos/logitech.jpg'},
+  { id: 2, name: "PHILIPS" ,imagePath:'logos/philips.png'},
+  { id: 3, name: "LinkedIn" ,imagePath:'logos/linkedin.png'},
+  { id: 4, name: "Amazon" ,imagePath:'logos/amazon.jpg'},
+  { id: 5, name: "Facebook" ,imagePath:'logos/facebook.png'},
+  { id: 5, name: "Google" ,imagePath:'logos/google.jpg'},
 ];
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ProductCard from "../../components/product-card";
+import Brand from "../../components/brand";
 
 function Home() {
   return (
     <div>
+      <Brand/>
       <ProductCard />
     </div>
   );
