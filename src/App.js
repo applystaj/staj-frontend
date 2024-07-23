25dd28f6e78ff30ddc5bceeebd65e1b4a2655f0b
Tolga Türkmen <165426383+tolgaturkmen01@users.noreply.github.com>
2024-07-23 11:44:55 +0300

    created shopping cart but not showing.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './index.css';
+import React from 'react';
+import Cart from './components/Cart';
+import './index.css'; // Tailwind CSS'i içeri aktarın
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="min-h-screen bg-gray-100 p-4">
+      <Cart />
     </div>
   );
 }
