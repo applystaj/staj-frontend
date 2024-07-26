51883746ec8fd275459f1213e7ac61e67c09ffe5
İLKNUR DONDURMA <dondurmailknur001@gmail.com>
2024-07-26 11:09:08 +0300

    update after merge

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProductCard from "../../components/product-card";
 import Brand from "../../components/brand";
 import ShowCase from "../../sector/showcase";
-import Header from "../../layout/index";
+import Header from "../../layout/header";
 
 function Home() {
   return (
